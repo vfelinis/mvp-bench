fbea8159439175cf4d9c3c46d476a960a93c202a
ponomarev.mike <ponomarev.mike@sharp-dev.net>
2021-02-13 14:53:59 +0500

    Add project files.

---
+++ mvp.bench/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mvp.bench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
